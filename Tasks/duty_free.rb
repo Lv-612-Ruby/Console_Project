e8a42b731b1f86046448bb06aca007de5b668188
halyna-m <galya.mik16@gmail.com>
2021-08-03 09:44:47 +0200

    Modified duty free file

--- Tasks/duty_free.rb
+++ Tasks/duty_free.rb
@@ -6,6 +6,6 @@ def duty_free(price, discount, holiday_cost)
 end
 
 puts duty_free(12, 50, 1000) 
-               # => 166
-puts duty_free(17, 10, 500)  
-               # => 294
+# => 166
+puts duty_free(17, 10, 500)              
+# => 294
